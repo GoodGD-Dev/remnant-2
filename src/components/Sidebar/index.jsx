eace1d397a955316d32ce6ac6248e6b84b9bad39
GoodGD <Dionatha.work@gmail.com>
2024-10-11 15:32:38 -0300

    feito sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
-import { Nav, Dropdown } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Nav, Dropdown, Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';  // Importando o arquivo CSS externo
 
 const Sidebar = ({ menuItems, setContent }) => {
+  const [activeIndex, setActiveIndex] = useState(0); // Inicializa com a tab 1 ativa por padrão
+
+  const handleSelect = (index, value) => {
+    setActiveIndex(index);
+    setContent(value);
+  };
+
   return (
-    <div className="sidebar d-flex flex-column bg-light p-3 vh-100 shadow-lg" style={{ width: '250px' }}>
-      <h5 className="sidebar-title mb-4 text-primary">Menu</h5>
+    <div className="sidebar d-flex flex-column p-3 vh-100 shadow-sm">
+      <h5 className="sidebar-title mb-4 text-dark">Menu</h5>
       <Nav className="flex-column">
         {menuItems.map((item, index) => (
           item.subOptions ? (
-            // Dropdown for items with subOptions
             <Dropdown key={index} className="mb-2">
-              <Dropdown.Toggle className="text-dark mb-2 px-3 py-2 rounded hover-link bg-light border-0">
+              <Dropdown.Toggle 
+                className={`dropdown-toggle-custom ${activeIndex === index ? 'active-item' : ''}`}>
                 {item.label}
               </Dropdown.Toggle>
               <Dropdown.Menu>
                 {item.subOptions.map((subItem, subIndex) => (
                   <Dropdown.Item
                     key={subIndex}
-                    onClick={() => setContent(subItem.value)}>
+                    onClick={() => handleSelect(index, subItem.value)}
+                    className="dropdown-item-custom">
                     {subItem.label}
                   </Dropdown.Item>
                 ))}
               </Dropdown.Menu>
             </Dropdown>
           ) : (
-            // Regular Nav.Link for items without subOptions
             <Nav.Item key={index}>
               <Nav.Link
-                onClick={() => setContent(item.value)}
-                className="text-dark mb-2 px-3 py-2 rounded hover-link">
+                onClick={() => handleSelect(index, item.value)}
+                className={`nav-link-custom ${activeIndex === index ? 'active-item' : ''}`}>
                 {item.label}
               </Nav.Link>
             </Nav.Item>
